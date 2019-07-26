b910ef62321232aa00316b793cc0ee046da149dc
eriskooo <lorman@pobox.sk>
2019-07-26 10:12:38 +0200

    eureka 2

--- currency-exchange-service/src/main/java/sk/lorman/springboot/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/sk/lorman/springboot/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,8 +2,11 @@ package sk.lorman.springboot.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
